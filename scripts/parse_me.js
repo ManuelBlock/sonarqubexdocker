61a72115b93eae482e31b9839b96171d70e79f8d
MANUEL JESUS GUTIERREZ FERNANDEZ <manuel.jesus.gutierrez.fernandez.st@everis.com>
2020-12-02 16:35:16 +0100

    solution and cwe added

--- scripts/parse_me.js
+++ scripts/parse_me.js
@@ -8,6 +8,41 @@ var outputJson = [];
 
 loop();
 
+async function getSolutionsCWE() {
+    return new Promise(function(resolve, reject) {
+        var req_complete = 0;
+        for (let index = 0; index < outputJson.length; index++) {
+            const element = outputJson[index];
+            var url = `http://localhost:9000/api/hotspots/show?hotspot=${element.key}`;
+            https.get(url, (resp) => {
+                let data = '';
+                // A chunk of data has been recieved.
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+                // The whole response has been received. Print out the result.
+                resp.on('end', () => {
+                    var res = JSON.parse(data);
+                    element.solution = res.rule.fixRecommendations.replace(/<\/?[^>]+(>|$)/g, "");;
+
+                    let er = /CWE-(\d+)/g
+                    element.cwe = res.rule.fixRecommendations.match(er);
+
+                    req_complete += 1;
+
+                    if (req_complete == outputJson.length) {
+                        resolve()
+                    }
+                });
+
+            }).on("error", (err) => {
+                console.log("Error: " + err.message);
+            });
+        }
+    })
+}
+
 async function loop() {
     response = await getPageFromSonarQube(page);
     outputJson = outputJson.concat(response.hotspots);
@@ -15,6 +50,7 @@ async function loop() {
         page += 1;
         loop();
     } else {
+        await getSolutionsCWE()
         fs.writeFile('allRecords.json', JSON.stringify(outputJson), function(err) {
             if (err) return console.log(err);
         });
