d36ad56325de7b69b82a781961b6ba077d6cafb8
MANUEL JESUS GUTIERREZ FERNANDEZ <manuel.jesus.gutierrez.fernandez.st@everis.com>
2020-12-14 18:06:40 +0100

    parser afinado

--- scripts/parse_me.js
+++ scripts/parse_me.js
@@ -1,4 +1,4 @@
-const https = require('http');
+var request = require('request');
 const fs = require('fs')
 
 var projectKey = "CIBERSEG";
@@ -14,33 +14,29 @@ async function getSolutionsCWE() {
         for (let index = 0; index < outputJson.length; index++) {
             const element = outputJson[index];
             var url = `http://localhost:9000/api/hotspots/show?hotspot=${element.key}`;
-            https.get(url, (resp) => {
-                let data = '';
-                // A chunk of data has been recieved.
-                resp.on('data', (chunk) => {
-                    data += chunk;
-                });
+            var options = {
+                'method': 'GET',
+                'url': url,
+                'headers': {
+                    'Authorization': 'Basic YWRtaW46YWRtaW4='
+                }
+            };
+            request(options, function(error, response) {
+                if (error) throw new Error(error);
+                var res = JSON.parse(response.body);
+                element.solution = res.rule.fixRecommendations.replace(/<\/?[^>]+(>|$)/g, "");
 
-                // The whole response has been received. Print out the result.
-                resp.on('end', () => {
-                    var res = JSON.parse(data);
-                    element.solution = res.rule.fixRecommendations.replace(/<\/?[^>]+(>|$)/g, "");
+                let er = /CWE-(\d+)/g
+                element.cwe = res.rule.fixRecommendations.match(er);
 
-                    let er = /CWE-(\d+)/g
-                    element.cwe = res.rule.fixRecommendations.match(er);
+                req_complete += 1;
 
-                    req_complete += 1;
-
-                    if (req_complete == outputJson.length) {
-                        resolve()
-                    }
-                });
-
-            }).on("error", (err) => {
-                console.log("Error: " + err.message);
+                if (req_complete == outputJson.length) {
+                    resolve()
+                }
             });
         }
-    })
+    });
 }
 
 async function loop() {
@@ -60,22 +56,17 @@ async function loop() {
 async function getPageFromSonarQube(nextPage) {
     return new Promise(function(resolve, reject) {
         var url = `http://localhost:9000/api/hotspots/search?projectKey=${projectKey}&p=${nextPage}&ps=500`;
-        https.get(url, (resp) => {
-            let data = '';
-
-            // A chunk of data has been recieved.
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
-
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-                var res = JSON.parse(data);
-                resolve(res)
-            });
-
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
+        var options = {
+            'method': 'GET',
+            'url': url,
+            'headers': {
+                'Authorization': 'Basic YWRtaW46YWRtaW4='
+            }
+        };
+        request(options, function(error, response) {
+            if (error) throw new Error(error);
+            var res = JSON.parse(response.body);
+            resolve(res)
         });
     })
 }
